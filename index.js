fa30e63b887b6e33495b5054f1cd40606b37ee77
Victor Tran <victor.tran@tempestas.io>
2022-04-16 22:39:22 -0400

    initial commit with deck creation

---
+++ index.js
@@ -0,0 +1,43 @@
+const deck = [];
+const suites = {};
+
+function createSuites() {
+    let suiteList = ["Heart", "Diamond", "Clover", "Spade"];
+
+    for (let i = 0; i < 4; i++) {
+        suites[i] = suiteList[i];
+    }
+}
+
+function generateCardValue(value = 0) {
+    const cardValue = ["A", "J", "Q", "K"];
+    if (value < 1 || value > 9) {
+        // 10, 11, 12 => 1, 2, 3
+        if (value > 9) {
+            return cardValue[value - 9];
+        }
+
+        return cardValue[value];
+    }
+    return value;
+}
+
+function createDeck() {
+    for (let i = 0; i < 13; i++) {
+        for (let j = 0; j < 4; j++) {
+            const card = generateCardValue(i);
+            deck[i * 4 + j] = {
+                card,
+                suite: suites[j],
+            };
+        }
+    }
+}
+
+function setUp() {
+    createSuites();
+    createDeck();
+}
+
+setUp();
+console.log(deck);
